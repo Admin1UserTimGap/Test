7c5b25508a041f0a41c681b847c36e0f8785dc18
Admin1UserTimGap <148370595+Admin1UserTimGap@users.noreply.github.com>
2025-04-17 14:40:46 +0000

    Pending changes exported from your codespace

--- tab.js
+++ tab.js
@@ -1,3 +1,48 @@
-function displaytab () {
-    
+let TABS=document.querySelectorAll(".tab");
+let ACTIVETABS=[]
+
+function AppendTabListener(TabID) {
+    ACTIVETABS.push(0)
+    let Tab=document.getElementById(TabID);
+    Tab.addEventListener("click", function () {
+        for (let i=0; i<ACTIVETABS.length; i++) {
+            if (ACTIVETABS[i]==1) {
+                ACTIVETABS[i]=0;
+            }
+            if (TABS[i].id==TabID) {
+                ACTIVETABS[i]=1;
+            }
+        }
+    })
+}
+
+function UpdateVisibility(elementID, state) {
+    if (state==1) {
+        document.getElementById(elementID).style.display="block";
+    }
+    else if (state==0) {
+        document.getElementById(elementID).style.display="none";
+    }
+    else {
+        console.error("Invalid state provided to UpdateVisibility");
+    }
+}
+
+function UpdateTabActive() {
+    let TabActive=null;
+    i=0;
+    ACTIVETABS.forEach(function (tab) {
+        if (tab==1) {
+            TabActive=TABS[i];
+        }
+        i++;
+    });
+    TABS.forEach(function (tab) {
+        tab.classList.remove("active");
+    });
+    TabActive.classList.add("active");
+}
+
+function Update() {
+    UpdateTabActive();
 }
